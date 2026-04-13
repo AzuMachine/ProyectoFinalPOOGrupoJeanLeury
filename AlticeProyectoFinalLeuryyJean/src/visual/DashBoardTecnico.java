1e086454c2ab226c8cc5fb0c4acd029258fd35e9
jeanm <jeanm@07-07-2006>
2026-04-13 10:37:06 -0400

    DashBoarTecnico

--- AlticeProyectoFinalLeuryyJean/src/visual/DashBoardTecnico.java
+++ AlticeProyectoFinalLeuryyJean/src/visual/DashBoardTecnico.java
@@ -1,10 +1,18 @@
 package visual;
 
 import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import logico.Altice;
 
 public class DashBoardTecnico extends JFrame {
 
@@ -31,6 +39,29 @@ public class DashBoardTecnico extends JFrame {
 	 * Create the frame.
 	 */
 	public DashBoardTecnico() {
+		
+		//Para el guardado
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				FileOutputStream altSalidaTecnico;
+				ObjectOutputStream altWriteTecnico;
+				
+				try {
+					altSalidaTecnico = new FileOutputStream("Altice.dat");
+					altWriteTecnico = new ObjectOutputStream(altSalidaTecnico);
+					altWriteTecnico.writeObject(Altice.getInstance());
+				} catch (FileNotFoundException e1) {
+					
+					e1.printStackTrace();
+				}catch (IOException e1) {
+					
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
